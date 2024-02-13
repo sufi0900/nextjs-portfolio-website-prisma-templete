86893da34020449a13127c3a5cc699a6ebdb32a7
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2024-02-14 00:14:15 +0500

    Update Reactions.tsx

--- apps/enji.dev/src/components/Reactions.tsx
+++ apps/enji.dev/src/components/Reactions.tsx
@@ -99,23 +99,20 @@ function Reactions({
     isLoading,
     data: {
       meta: {
-        views = 0,
-        shares = 0,
-        reactions = 0,
-        reactionsDetail: { THINKING = 0, CLAPPING = 0, AMAZED = 0 } = {},
+        views,
+        shares,
+        reactions,
+        reactionsDetail: { THINKING, CLAPPING, AMAZED } = {},
       } = {},
-      metaUser: { reactionsDetail: userReactionsDetail = {} } = {},
+      metaUser: { reactionsDetail: user } = {},
     } = {},
     addShare,
     addReaction,
   } = useInsight({ slug, contentType, contentTitle, countView: withCountView });
 
-  const CLAPPING_QUOTA =
-    MAX_REACTIONS_PER_SESSION - (userReactionsDetail?.CLAPPING ?? 0);
-  const THINKING_QUOTA =
-    MAX_REACTIONS_PER_SESSION - (userReactionsDetail?.THINKING ?? 0);
-  const AMAZED_QUOTA =
-    MAX_REACTIONS_PER_SESSION - (userReactionsDetail?.AMAZED ?? 0);
+  const CLAPPING_QUOTA = MAX_REACTIONS_PER_SESSION - (user?.CLAPPING ?? 0);
+  const THINKING_QUOTA = MAX_REACTIONS_PER_SESSION - (user?.THINKING ?? 0);
+  const AMAZED_QUOTA = MAX_REACTIONS_PER_SESSION - (user?.AMAZED ?? 0);
 
   const controls = useAnimationControls();
 
